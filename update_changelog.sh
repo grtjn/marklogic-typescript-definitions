d9314db2a3a80ff8bdcfb1b2c6e3212023a63223
Geert Josten <geert.josten@marklogic.com>
2015-09-21 18:43:02 +0200

    Fixed CHANGELOG script

--- update_changelog.sh
+++ update_changelog.sh
@@ -1,1 +1,1 @@
-github_changelog_generator -u grtjn -p marklogic-typescript-definitions -t ef870f398ef5ef1deed9967853716a030ccaa544
+github_changelog_generator -u grtjn -p marklogic-typescript-definitions
